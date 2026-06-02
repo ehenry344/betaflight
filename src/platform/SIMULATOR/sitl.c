11e7a77045a0aeaef73222382dc5588bbea552b8
Evan <ehenry344@gmail.com>
2026-06-01 21:50:25 -0400

    begin breaking out sitl files

--- src/platform/SIMULATOR/sitl.c
+++ src/platform/SIMULATOR/sitl.c
@@ -769,62 +769,6 @@ pwmOutputPort_t *pwmGetMotors(void)
     return pwmMotors;
 }
 
-static float pwmConvertFromExternal(uint16_t externalValue)
-{
-    return (float)externalValue;
-}
-
-static uint16_t pwmConvertToExternal(float motorValue)
-{
-    return (uint16_t)motorValue;
-}
-
-static void pwmDisableMotors(void)
-{
-    // NOOP
-}
-
-static void pwmWriteMotor(uint8_t index, float value)
-{
-    if (index < MAX_SUPPORTED_MOTORS) {
-        motorsPwm[index] = value - idlePulse;
-    }
-
-    if (index < pwmRawPkt.motorCount) {
-        pwmRawPkt.pwm_output_raw[index] = value;
-    }
-}
-
-static void pwmWriteMotorInt(uint8_t index, uint16_t value)
-{
-    pwmWriteMotor(index, (float)value);
-}
-
-static void pwmShutdownPulsesForAllMotors(void)
-{
-    // NOOP
-}
-
-static void pwmCompleteMotorUpdate(void)
-{
-    // send to simulator
-    // for gazebo8 ArduCopterPlugin remap, normal range = [0.0, 1.0], 3D rang = [-1.0, 1.0]
-
-    double outScale = 1000.0;
-    if (featureIsEnabled(FEATURE_3D)) {
-        outScale = 500.0;
-    }
-
-    for (int i = 0; i < 4; i++) {
-        pwmPkt.motor_speed[i] = motorsPwm[i] / outScale;
-    }
-
-    // get one "fdm_packet" can only send one "servo_packet"!!
-    if (pthread_mutex_trylock(&updateLock) != 0) return;
-    udpSend(&pwmLink, &pwmPkt, sizeof(servo_packet));
-    udpSend(&pwmRawLink, &pwmRawPkt, sizeof(servo_packet_raw));
-}
-
 void servoWrite(uint8_t index, float value)
 {
     servosPwm[index] = value;
